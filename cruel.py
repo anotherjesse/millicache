4739e8722f4f62a441bee4807b139a2ba48a1ac0
Jesse Andrews <anotherjesse@gmail.com>
2012-03-08 18:30:10 -0800

    add some heapness

--- cruel.py
+++ cruel.py
@@ -5,7 +5,25 @@
 # E.xpirey
 # L.east
 
+import heapq
 import time
+
+
+class Cell(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.access = time.time()
+
+    def __cmp__(self, other):
+        return cmp(self.access, other.access)
+
+    def touch(self):
+        self.access = time.time()
+
+    @property
+    def expired(self):
+        pass
 
 
 class Client(object):
@@ -13,28 +31,57 @@ class Client(object):
 
     def __init__(self, max_size=32, *args, **kwargs):
         """Ignores the passed in args."""
-        self.__db = {}
-        self.__max_size = 32
-        self.__lru = []
-        self.__expire = []
+        self.__max_size = max_size
+        self._init_db()
 
     def get(self, key):
-        """Retrieves the value for a key or None.
-
-        this expunges expired keys during each get"""
-        for key in list(self.__db.iterkeys()):
-            (expires, value) = self.__db.get(key, (0, None))
-            if expires and time.time() >= expires:
-                del self.__db[key]
-        return self.__db.get(key, (0, None))[1]
+        """Retrieves the value for a key or None."""
+        # for key in list(self.__db.iterkeys()):
+        #     (expires, value) = self.__db.get(key, (0, None))
+        #     if expires and time.time() >= expires:
+        #         del self.__db[key]
+        cell = self.__db.get(key)
+        if cell:
+            if cell.expired:
+                self._delete(cell.key)
+            else:
+                cell.touch()
+                return cell.value
 
     def set(self, key, value, time=0, min_compress_len=0):
         """Sets the value for a key."""
-        timeout = 0
-        if time != 0:
-            timeout = time.time() + time
-        self.__db[key] = (timeout, value)
-        return True
+        if key in self.__db:
+            cell = self.__db[key]
+            cell.value = value
+            cell.touch()
+            heapq.heapify(self.__lru)
+        else:
+            self._prepare_for_insert()
+            cell = Cell(key, value)
+            self.__db[key] = cell
+            heapq.heappush(self.__lru, cell)
+
+    @property
+    def db(self):
+        return self.__db
+
+    def _init_db(self):
+        self.__db = {}
+        self.__lru = []
+        self.__expire = []
+
+    def _prepare_for_insert(self):
+        if len(self.__db) >= self.__max_size:
+            cell = heapq.heappop(self.__lru)
+            del self.__db[cell.key]
+
+    def _delete(self, key):
+        '''remove a given key from datastore'''
+        cell = self.__db.get(key)
+        if cell:
+            del self.__db[key]
+            self.__lru.remove(cell)
+            heapq.heapify(self.__lru)
 
     def add(self, key, value, time=0, min_compress_len=0):
         """Sets the value for a key if it doesn't exist."""
@@ -53,6 +100,25 @@ class Client(object):
 
 
 if __name__ == '__main__':
-    c = Client()
+    c = Client(max_size=10)
     c.set(1, 'expected')
     assert c.get(1) == 'expected'
+
+    # check that we only keep most recent 10 items
+    for i in xrange(100):
+        c.set(i, pow(i, 2))
+    assert c.get(1) == None
+
+    # getting an item ensures it isn't removed via lru
+    for i in xrange(100):
+        c.set(i, pow(i, 2))
+        c.get(1)
+    assert c.get(1) == 1
+
+    # setting an item ensures it isn't removed via lru
+    for i in xrange(100):
+        c.set(i, pow(i, 2))
+        c.set(1, 1)
+    assert c.get(1) == 1
+
+    print c.db.keys()
