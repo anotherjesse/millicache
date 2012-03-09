2610ba0945daffbc4bb69e39a09d4d569f4d907d
Jesse Andrews <anotherjesse@gmail.com>
2012-03-08 18:41:06 -0800

    initial work on expiry

--- cruel.py
+++ cruel.py
@@ -10,20 +10,30 @@ import time
 
 
 class Cell(object):
-    def __init__(self, key, value):
+    def __init__(self, key, value, timeout=None):
         self.key = key
-        self.value = value
+        self.update(value, timeout)
+
+    def update(self, value, timeout=None):
         self.access = time.time()
+        self.value = value
+        if timeout:
+            self._expires = time.time() + timeout
+        else:
+            self._expires = None
 
     def __cmp__(self, other):
         return cmp(self.access, other.access)
+
+    def __repr__(self):
+        return "<cached[%s] = %s>" % (self.key, self.value)
 
     def touch(self):
         self.access = time.time()
 
     @property
     def expired(self):
-        pass
+        return self._expires and time.time() >= self._expires
 
 
 class Client(object):
@@ -36,10 +46,6 @@ class Client(object):
 
     def get(self, key):
         """Retrieves the value for a key or None."""
-        # for key in list(self.__db.iterkeys()):
-        #     (expires, value) = self.__db.get(key, (0, None))
-        #     if expires and time.time() >= expires:
-        #         del self.__db[key]
         cell = self.__db.get(key)
         if cell:
             if cell.expired:
@@ -52,12 +58,12 @@ class Client(object):
         """Sets the value for a key."""
         if key in self.__db:
             cell = self.__db[key]
-            cell.value = value
+            cell.update(value, time)
             cell.touch()
             heapq.heapify(self.__lru)
         else:
             self._prepare_for_insert()
-            cell = Cell(key, value)
+            cell = Cell(key, value, time)
             self.__db[key] = cell
             heapq.heappush(self.__lru, cell)
 
@@ -83,42 +89,52 @@ class Client(object):
             self.__lru.remove(cell)
             heapq.heapify(self.__lru)
 
-    def add(self, key, value, time=0, min_compress_len=0):
-        """Sets the value for a key if it doesn't exist."""
-        if not self.get(key) is None:
-            return False
-        return self.set(key, value, time, min_compress_len)
+    # def add(self, key, value, time=0, min_compress_len=0):
+    #     """Sets the value for a key if it doesn't exist."""
+    #     if not self.get(key) is None:
+    #         return False
+    #     return self.set(key, value, time, min_compress_len)
 
-    def incr(self, key, delta=1):
-        """Increments the value for a key."""
-        value = self.get(key)
-        if value is None:
-            return None
-        new_value = int(value) + delta
-        self.__db[key] = (self.__db[key][0], str(new_value))
-        return new_value
+    # def incr(self, key, delta=1):
+    #     """Increments the value for a key."""
+    #     value = self.get(key)
+    #     if value is None:
+    #         return None
+    #     new_value = int(value) + delta
+    #     self.__db[key] = (self.__db[key][0], str(new_value))
+    #     return new_value
 
 
 if __name__ == '__main__':
     c = Client(max_size=10)
+
+    c._init_db()
     c.set(1, 'expected')
     assert c.get(1) == 'expected'
+    assert c.get(2) == None
 
+    c._init_db()
     # check that we only keep most recent 10 items
     for i in xrange(100):
         c.set(i, pow(i, 2))
     assert c.get(1) == None
 
+    c._init_db()
     # getting an item ensures it isn't removed via lru
     for i in xrange(100):
         c.set(i, pow(i, 2))
         c.get(1)
     assert c.get(1) == 1
 
+    c._init_db()
     # setting an item ensures it isn't removed via lru
     for i in xrange(100):
         c.set(i, pow(i, 2))
         c.set(1, 1)
     assert c.get(1) == 1
 
-    print c.db.keys()
+    c._init_db()
+    # don't return expired keys
+    c.set(1, 'past', -1)
+    print c.db
+    assert c.get(1) == None
